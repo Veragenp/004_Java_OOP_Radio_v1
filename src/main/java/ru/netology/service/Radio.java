f79359dc59dcd9681b1b785f09b074896c219a96
Yury <yp@unotex.ru>
2022-05-17 14:34:43 +0700

    correct pom.xml

--- src/main/java/ru/netology/service/Radio.java
+++ src/main/java/ru/netology/service/Radio.java
@@ -1,2 +1,98 @@
-package ru.netology.service;public class Radio {
+package ru.netology.service;
+
+public class Radio {
+    // завести поля номер текущей радиостанции numberCurrentRadio
+    // громкость звука soundVolumeCurrent
+    // soundVolumeMin = 0
+    // soundVolumeMax = 9
+    // nextRadio PreviousRadio
+    // если текущая радиостанция 9 и клиент нажал кнопку Next то текущей должна стать 0 я, если 0 то предыдущая 9
+    //Должен иметь возможность выставлять номер радиостанции через прямое указание номера
+    // numberMinRadio = 0
+    // numberMaxRadio = 10
+    // если достиг максимума (минимума), то дальнейшее нажатие ни к чему не приводит
+    private String name;
+    private int soundVolumeCurrent;
+    private int soundVolumeMin;
+    private int soundVolumeMax;
+    private int numberCurrentRadio;
+    private int numberMinRadio;
+    private int numberMaxRadio;
+    private int nextRadio;
+    private int PreviousRadio;
+
+
+    public int getSoundVolumeCurrent() {
+        return soundVolumeCurrent;
+    }
+
+    public void setSoundVolumeCurrent(int soundVolumeCurrent) {
+        this.soundVolumeCurrent = soundVolumeCurrent;
+    }
+
+
+    public void setSoundVolumeMin(int soundVolumeMin) {
+        this.soundVolumeMin = soundVolumeMin;
+    }
+
+    public void increaseSoundVolume() {
+        if (soundVolumeCurrent < soundVolumeMax) {
+            soundVolumeCurrent = soundVolumeCurrent + 1;
+        }
+    }
+
+    public void turnDownSoundVolume() {
+        if (soundVolumeCurrent > soundVolumeMin) {
+            soundVolumeCurrent = soundVolumeCurrent - 1;
+        }
+    }
+
+
+    public void setSoundVolumeMax(int soundVolumeMax) {
+        this.soundVolumeMax = soundVolumeMax;
+    }
+
+    public int getNumberCurrentRadio() {
+        return numberCurrentRadio;
+    }
+
+    public void setNumberCurrentRadio(int numberCurrentRadio) { // создать тест
+        if (numberCurrentRadio > numberMaxRadio) {
+            return;
+        }
+        if (numberCurrentRadio < numberMinRadio) {
+            return;
+        }
+        this.numberCurrentRadio = numberCurrentRadio;
+    }
+
+    public void setNumberMinRadio(int numberMinRadio) {
+        this.numberMinRadio = numberMinRadio;
+    }
+
+    public void setNumberMaxRadio(int numberMaxRadio) {
+        this.numberMaxRadio = numberMaxRadio;
+    }
+
+    public int nextRadio() {
+        if (numberCurrentRadio <= numberMaxRadio) {
+            numberCurrentRadio = numberCurrentRadio + 1;
+
+        }
+        if (numberCurrentRadio > numberMaxRadio) {
+            numberCurrentRadio = numberMinRadio;
+        }
+        return numberCurrentRadio;
+    }
+
+    public int previousRadio() {
+        int channel = 0;
+        if (numberCurrentRadio >= numberMinRadio) {
+            numberCurrentRadio = numberCurrentRadio - 1;
+        }
+        if (numberCurrentRadio < numberMinRadio) {
+            numberCurrentRadio = numberMaxRadio;
+        }
+        return numberCurrentRadio;
+    }
 }
--- src/test/java/ru/netology/service/RadioTest.java
+++ src/test/java/ru/netology/service/RadioTest.java
@@ -1,4 +1,130 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RadioTest {
-  
+
+    @Test
+    void shouldNotIncreaseSoundVolumeMoreThanMax() {
+        Radio radio = new Radio();
+        radio.setSoundVolumeMax(10);
+        radio.setSoundVolumeMin(0);
+        radio.setSoundVolumeCurrent(10);
+        radio.increaseSoundVolume();
+        assertEquals(10, radio.getSoundVolumeCurrent());
+    }
+
+    @Test
+    void shouldNotTurnDownSoundVolumeLassThanMin() {
+        Radio radio = new Radio();
+        radio.setSoundVolumeMax(10);
+        radio.setSoundVolumeMin(0);
+        radio.setSoundVolumeCurrent(0);
+        radio.turnDownSoundVolume();
+        assertEquals(0, radio.getSoundVolumeCurrent());
+    }
+
+    @Test
+    void shouldIncreaseSoundVolume() {
+        Radio radio = new Radio();
+        radio.setSoundVolumeMax(10);
+        radio.setSoundVolumeMin(0);
+        radio.setSoundVolumeCurrent(9);
+        radio.increaseSoundVolume();
+        assertEquals(10, radio.getSoundVolumeCurrent());
+    }
+
+    @Test
+    void shouldTurnDownSoundVolume() {
+        Radio radio = new Radio();
+        radio.setSoundVolumeMax(10);
+        radio.setSoundVolumeMin(0);
+        radio.setSoundVolumeCurrent(1);
+        radio.turnDownSoundVolume();
+        assertEquals(0, radio.getSoundVolumeCurrent());
+    }
+
+
+    @Test
+    void shouldSetNumberCurrentRadioEqualMax() {
+        Radio radio = new Radio();
+        radio.setNumberMaxRadio(9);
+        radio.setNumberMinRadio(0);
+        radio.setNumberCurrentRadio(9);
+        assertEquals(9, radio.getNumberCurrentRadio());
+    }
+
+    @Test
+    void shouldSetNumberCurrentRadioEqualMin() {
+        Radio radio = new Radio();
+        radio.setNumberMaxRadio(9);
+        radio.setNumberMinRadio(0);
+        radio.setNumberCurrentRadio(0);
+        assertEquals(0, radio.getNumberCurrentRadio());
+    }
+
+    @Test
+    void shouldSetNumberCurrentRadioAboveMax() {
+        Radio radio = new Radio();
+        radio.setNumberMaxRadio(9);
+        radio.setNumberMinRadio(0);
+        radio.setNumberCurrentRadio(10);
+        assertEquals(0, radio.getNumberCurrentRadio());
+    }
+
+    @Test
+    void shouldSetNumberCurrentRadioBelowMin() {
+        Radio radio = new Radio();
+        radio.setNumberMaxRadio(9);
+        radio.setNumberMinRadio(0);
+        radio.setNumberCurrentRadio(-1);
+        assertEquals(0, radio.getNumberCurrentRadio());
+    }
+
+
+    @Test
+    void shouldSwitchNextRadio() {
+        Radio radio = new Radio();
+        radio.setNumberMaxRadio(9);
+        radio.setNumberMinRadio(0);
+        radio.setNumberCurrentRadio(8);
+        radio.nextRadio();
+        assertEquals(9, radio.getNumberCurrentRadio());
+
+    }
+
+    @Test
+    void shouldNotSwitchAboveMaxRadio() {
+        Radio radio = new Radio();
+        radio.setNumberMaxRadio(9);
+        radio.setNumberMinRadio(0);
+        radio.setNumberCurrentRadio(9);
+        radio.nextRadio();
+        assertEquals(0, radio.getNumberCurrentRadio());
+
+    }
+
+    @Test
+    void shouldNotSwitchBelowMinRadio() {
+        Radio radio = new Radio();
+        radio.setNumberMaxRadio(9);
+        radio.setNumberMinRadio(0);
+        radio.setNumberCurrentRadio(0);
+        radio.previousRadio();
+        assertEquals(9, radio.getNumberCurrentRadio());
+
+    }
+
+    @Test
+    void shouldSwitchBelowRadio() {
+        Radio radio = new Radio();
+        radio.setNumberMaxRadio(9);
+        radio.setNumberMinRadio(0);
+        radio.setNumberCurrentRadio(1);
+        radio.previousRadio();
+        assertEquals(0, radio.getNumberCurrentRadio());
+
+    }
 }
